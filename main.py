f59da7901d3dcb1556a322bde8d82190fb8388c6
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-24 00:37:06 +0000

    Block Scope does not exist in Python.

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 ################### Scope ####################
 
-enemies = 1
+# enemies = 1
 
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
 
-#Global Scope
-player_health = 10
+# #Global Scope
+# player_health = 10
 
-# Local Scope
+# # Local Scope
 
-def game():
-  def drink_potion():
-    potion_strength = 2
-    print(player_health)
+# def game():
+#   def drink_potion():
+#     potion_strength = 2
+#     print(player_health)
 
-  drink_potion()
+#   drink_potion()
 
   
-print(player_health)
+# print(player_health)
+
+# There is no Block Scope
+
+game_level = 3
+def create_enemy():
+  enemies = ["Skeleton", "Zombie", "Alien"]
+  if game_level < 5:
+    new_enemy = enemies[0]
+
+  print(new_enemy)
+
+create_enemy()
+
